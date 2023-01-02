4d1e6d76b591c2c6632f3119ece686d7eb65448c
J319s0 <josh.smith3190@gmail.com>
2023-01-02 18:00:20 +0800

    generic css to tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors:
+		{
+			"background": "#121212",
+			"card": "#1e1e1e",
+			"white": "#FFFFFF"
+		},
+		extend: {},
+	},
+	plugins: [],
+}
