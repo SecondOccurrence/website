f038e89d7734abd9616ecc3f92f22e4ed970c47e
Josh S <100011785+SecondOccurrence@users.noreply.github.com>
2023-01-10 16:31:11 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://secondoccurrence.live',
   integrations: [mdx(), sitemap(), tailwind()]
 });
